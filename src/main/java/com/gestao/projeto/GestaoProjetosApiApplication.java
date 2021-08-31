cf58f2e1002fbdefd10f135274b2294ae19b6231
afarias <arao.alves7@gmail.com>
2021-08-31 13:02:40 -0300

    Subindo api

---
+++ src/main/java/com/gestao/projeto/GestaoProjetosApiApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProjetosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProjetosApiApplication.class, args);
+	}
+
+}
